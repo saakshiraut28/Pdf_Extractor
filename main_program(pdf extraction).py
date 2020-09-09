ab27204122d47fde563210aa0fa6121595859d69
Saakshi Raut <saakshiraut28@gmail.com>
2020-09-09 10:30:04 +0530

    Add files via upload

---
+++ main_program(pdf extraction).py
@@ -0,0 +1,23 @@
+from PyPDF2 import PdfFileReader
+
+file=open('C:/Users/Owner/Documents/Virtualization in cc.pdf', "rb")
+
+reader=PdfFileReader(file)
+print("\nDocument INFO: ",(reader.getDocumentInfo()))
+
+print("\n__________________________________________________________________________________________________\n")
+
+print("\nPages: ",reader.getNumPages())
+
+print("\nPDF File created by: ",reader.getDocumentInfo().creator)
+
+print("\n__________________________________________________________________________________________________\n")
+
+pages=reader.getNumPages()
+for i in range(0,pages):
+    print("Page Number: ",i+1)
+    print("\n***\n")
+    pageobj = reader.getPage(i)
+    print(pageobj.extractText)
+    print("\n__________________________________________________________________________________________________\n")
+file.close()
